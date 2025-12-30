ddf870beebd8e3e7fb4defe22bf5e7b2fe59a3cc
hieunt09102019 <56328785+hieunt09102019@users.noreply.github.com>
2025-12-30 08:10:15 +0700

    Delete packages/agrid-react-native directory

--- packages/agrid-react-native/src/hooks/useAgrid.ts
+++
@@ -1,8 +0,0 @@
-import { Agrid } from '../agrid-rn'
-import React from 'react'
-import { AgridContext } from '../AgridContext'
-
-export const useAgrid = (): Agrid => {
-  const { client } = React.useContext(AgridContext)
-  return client
-}
